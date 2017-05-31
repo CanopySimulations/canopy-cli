932d3aaa327fe0150f50448302ec189b89bc7c78
James Thurley <mail@jamesthurley.com>
2017-05-31 15:38:42 +0100

    Support displaying results in tables

--- Canopy.Cli.Executable/Commands/ListConfigsCommand.cs
+++ Canopy.Cli.Executable/Commands/ListConfigsCommand.cs
@@ -1,6 +1,9 @@
 ﻿﻿using System;
 using System.Threading.Tasks;
 using Canopy.Api.Client;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 
 namespace Canopy.Cli.Executable.Commands
 {
@@ -17,10 +20,11 @@ namespace Canopy.Cli.Executable.Commands
 			Console.WriteLine("Requesting configs...");
 			var configClient = new ConfigClient(this.configuration);
 			var result = await configClient.GetConfigsAsyncAsync(this.authenticatedUser.TenantId, "car", null, null);
-			foreach (var item in result.QueryResults.Documents)
-			{
-				Console.WriteLine(item.Name);
-			}
+
+            Utilities.WriteTable(
+                new[] { "Name", "Id", "UserId" },
+                result.QueryResults.Documents.Select(v => new string[] { v.Name, v.DocumentId, v.UserId }));
 		}
+
     }
 }
---
+++ Canopy.Cli.Executable/Utilities.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Canopy.Cli.Executable
+{
+    public static class Utilities
+    {
+        public static void WriteTable(IEnumerable<string> headers, IEnumerable<IEnumerable<string>> values, int padding = 1)
+        {
+            var lines = new List<List<string>>
+            {
+                headers.ToList(),
+                headers.Select(v => string.Concat(Enumerable.Repeat("-", v?.Length ?? 0))).ToList()
+            }
+            .Concat(values.Select(v => v.ToList())).ToList();
+
+            // Calculate maximum numbers for each element accross all lines
+            var numElements = lines[0].Count;
+            var maxValues = new int[numElements];
+            for (int i = 0; i < numElements; i++)
+            {
+                maxValues[i] = lines.Max(x => (x.Count > i + 1 && x[i] != null ? x[i].Length : 0)) + padding;
+            }
+            var sb = new StringBuilder();
+            // Build the output
+            foreach (var line in lines)
+            {
+                sb.AppendLine();
+                sb.Append(" ");
+                for (int i = 0; i < line.Count; i++)
+                {
+                    var value = line[i];
+                    // Append the value with padding of the maximum length of any value for this element
+                    if (value != null)
+                    {
+                        sb.Append(value.PadRight(maxValues[i]));
+                    }
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+
+    }
+}
